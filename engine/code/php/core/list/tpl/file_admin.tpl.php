99c4a7446b1faeda7ca35942a4403d5b4c6bcdff
aldotripiciano <aldo.tripiciano@gmail.com>
2015-06-24 13:10:28 +0200

    stuff

--- engine/code/php/core/list/tpl/file_admin.tpl.php
+++ engine/code/php/core/list/tpl/file_admin.tpl.php
@@ -1,1 +1,4 @@
-<li class="list-item list-item-file list-item-file-admin file-[EXT]"><a class="file-link" href="[URL]">[TITLE]</a> <input type="text" size="25" value="[WIKI_TAG]" /> [USER_ACTIONS]</li>
+<li class="list-item list-item-file list-item-file-admin file-[EXT]">
+  <a class="file-link" href="[URL]">[TITLE]</a>
+  <input type="text" size="25" value="[WIKI_TAG]" /> [USER_ACTIONS]
+</li>
--- engine/code/php/core/shadow/js/shadow.js
+++ engine/code/php/core/shadow/js/shadow.js
@@ -1,24 +1,41 @@
 // Add/Edit page form.
 // TODO: this has all to be refactored.
 
+/**
+ * Page edit action. TODO: move in page module.
+ */
 function pageEdit(action) {
     openShadow({ module : 'node', context: action, type: 'tabs'});
+
 }
 
+/**
+ * Page delete action. TODO: move in page module.
+ */
 function pageDelete() {
     openShadow({ module : 'node', context: 'node_delete', type: 'single'});
 }
 
+/**
+ * Open a shadow (lightbox) with the specified parameters.
+ * @param shadow
+ */
 function openShadow(shadow) {
-    if (shadow.type == 'undefined') {
+    if (shadow.type == undefined) {
         shadow.type = 'tabs';
     }
+    console.log(shadow);
     $('#shadow-item').html('').attr('rel', shadow.context).load(
         '?shadow=' + JSON.stringify(shadow), function() {
-        $.getScript('/engine/code/php/core/file/js/jquery.fileupload.js');
-        $.getScript('/engine/code/php/core/file/js/file-upload.js');
+
         pageRefresh();
         refreshButtons();
+            // Include attached scripts if present.
+            if (shadow.attach != undefined) {
+                for (i = 0; i < shadow.attach.length; i++) {
+                    $.getScript(shadow.attach[i]);
+                }
+            }
         $('.shadow-title').find('a').on('click', function () {
             if (!($(this).parent().hasClass('enabled'))) {
                 $('.enabled').removeClass('enabled');
@@ -35,7 +52,6 @@ function openShadow(shadow) {
  * Submit a shadow form.
  */
 function shadowSubmit() {
-
     // TODO: this goes into ckeditor.
     if ($('textarea#content').length) {
     var editor = CKEDITOR.instances.content;
