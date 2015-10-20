0108c882741e6d53ba88f67ed7a5205ecf7693be
aldotripiciano <aldo.tripiciano@gmail.com>
2015-10-20 12:41:18 +0200

    Manager

--- engine/code/php/core/manager/js/manager.js
+++ engine/code/php/core/manager/js/manager.js
@@ -2,14 +2,23 @@
 function refreshManagerLeaves() {
     $('.manager-leaf').each(function() {
         $(this).unbind().bind('click', function() {
+
             var leaf = ($(this).attr('rel')).replace('leaf-', '');
             var leafobj = $(this);
-            var openurl = '/expand/?path=' + leaf + '&node=sabaudia';
-            $.ajax({
-                url: openurl
-            }).done(function(data) {
-                leafobj.parent().append(data);
-            });
+            leafobj.parent().find('ul').remove();
+            var expanded = $(this).parent().hasClass('expanded');
+            if (expanded) {
+                leafobj.parent().removeClass('expanded');
+            } else {
+                var openurl = '/expand/?path=' + leaf + '&node=sabaudia';
+                $.ajax({
+                    url: openurl
+                }).done(function(data) {
+                    leafobj.parent().append(data);
+                    leafobj.parent().addClass('expanded');
+                });
+            }
+            return FALSE;
         });
     });
 }
