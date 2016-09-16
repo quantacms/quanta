6e970cc7cbec141e0eb602ec284d5de6ef25bf82
aldotripiciano <aldo.tripiciano@gmail.com>
2016-09-16 17:24:36 +0200

    Engine

--- engine/code/php/boot.php
+++ engine/code/php/boot.php
@@ -4,11 +4,13 @@
   $env->addInclude('engine/code/css/engine.css');
   $env->addInclude('engine/code/js/lib/jquery.js');
   $env->addInclude('engine/code/js/engine.js');
+
   $env->addInclude('engine/code/js/lib/jquery.knob.js');
   $env->addInclude('engine/code/js/lib/jquery.ui.widget.js');
   $env->addInclude('engine/code/js/lib/jquery.iframe-transport.js');
   $env->addInclude('engine/code/js/lib/zebra.js');
   $env->addInclude('engine/code/css/zebra.css');
+
   $env->startSession();
   $env->runModules();
   $env->hook('boot');
@@ -17,20 +19,24 @@
   if (isset($_GET['cron'])) {
     $env->hook('cron');
   }
+
   if (isset($env->request_json->action)) {
     $env->hook('action_' . $env->request_json->action, array('data' => (array) $env->request_json));
   }
-  $env->hook('init');
+  $page = new Page($env, 'index.html');
+
+  $env->hook('init', array('page' => &$page));
   if ($env->getData('content') == NULL) {
     $env->setData('title', '404 - Page not found');
     $env->setData('content', '404 - Page not found. This page doesn\'t exist or has been removed!');
   }
 
-  $page = new Page($env, 'index.html', $env->getData('title'), $env->getData('content'));
+  $page->loadIncludes();
   $page->buildHTML();
 
   print $page->render();
 
   $env->hook('complete');
+
   exit();
 ?>
