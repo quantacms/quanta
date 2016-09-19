90fb40595ba7739820ce7062aec0cbdba48d1924
aldotripiciano <aldo.tripiciano@gmail.com>
2016-09-19 10:03:05 +0200

    Engine

---
+++ engine/code/php/core/zecommerce/tpl/products.tpl.php
@@ -0,0 +1,8 @@
+<div class="product">
+    <div class="product-thumb">[[NODE:name]:[NODE:thumbnail]]</div>
+    <div class="product-info">
+    <div class="product-name"><h3>[[NODE:name]:[NODE:title]]</h3></div>
+    <div class="product-price">&euro; 15.45</div>
+    <div class="product-availability product-available">DISPONIBILE</div>
+  </div>
+</div>
