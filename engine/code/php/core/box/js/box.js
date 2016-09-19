861e2ad1b0444efc93f622f47a787292554cf1a8
aldotripiciano <aldo.tripiciano@gmail.com>
2016-09-19 18:00:01 +0200

    ENGINE

--- engine/code/php/core/box/js/box.js
+++ engine/code/php/core/box/js/box.js
@@ -1,68 +1,42 @@
 var resizeBoxes = function() {
-		var margin;
-    var padding;
-    var borderWidth;
+
     $('.box').each(function() {
         var inner = $(this).find('.inner');
-        
-				// marginLeft = parseInt(inner.css('margin-left').replace("px", ""));
-        // marginRight = parseInt(inner.css('margin-right').replace("px", ""));
-				// paddingLeft = parseInt(inner.css('padding-left').replace("px", ""));
-        // paddingRight = parseInt(inner.css('padding-right').replace("px", ""));
-       
-				paddingTop = parseInt(inner.css('padding-top').replace("px", ""));
+
+	    paddingTop = parseInt(inner.css('padding-top').replace("px", ""));
         paddingBottom = parseInt(inner.css('padding-bottom').replace("px", ""));
-				
-				marginTop = parseInt(inner.css('margin-top').replace("px", ""));
+		marginTop = parseInt(inner.css('margin-top').replace("px", ""));
         marginBottom = parseInt(inner.css('margin-bottom').replace("px", ""));
+		var parentWidth = $(this).parent().innerWidth();
 
-				var parentWidth = $(this).parent().innerWidth();
-
-				borderTopWidth = parseInt(inner.css('border-top-width').replace("px", ""));
-				borderBottomWidth = parseInt(inner.css('border-bottom-width').replace("px", ""));
+		borderTopWidth = parseInt(inner.css('border-top-width').replace("px", ""));
+		borderBottomWidth = parseInt(inner.css('border-bottom-width').replace("px", ""));
        
-				var w = $(this).innerWidth();
-        var wratio;
-				var hratio;
-        var blockwidth;
+		var w = $(this).innerWidth();
+
+        // used for mobile rendering.
+        var wclass;
+
+        $(this).removeClass('w-75-100').removeClass('w-50-75').removeClass('w-25-50').removeClass('w-0-25');
+        if (w >= parentWidth / 100 * 75) {wclass = 'w-75-100'; }
+        else if (w >= parentWidth / 100 * 50) {wclass = 'w-50-75'; }
+        else if (w >= parentWidth / 100 * 25) {wclass = 'w-25-50'; }
+        else {wclass = 'w-0-25'; }
+
+
         // TODO: unpredictable, but we need to start with a 66% width
         // to calculate a height in line with the 33%.
-
-        if ($(this).hasClass('w67')) {
-            $(this).css('width', '67%');
-        }
-
-				if ($(this).hasClass('w100')) {	wratio = 100;	}
-        else if ($(this).hasClass('w75')) { wratio = 75; }
-				else if ($(this).hasClass('w50')) { wratio = 50; }
-				else if ($(this).hasClass('w33')) { wratio = 33; }
-				else if ($(this).hasClass('w25')) { wratio = 25; }
-			
-				if ($(this).hasClass('h100')) {
-            ratio = 1;
+        var hclass = $(this).attr("class").match(/h[0-9]*\b/);
+        if (hclass!= null && hclass[0]) {
+            ratio = parseInt(100 / hclass[0].replace('h', ''));
             h = parentWidth / ratio;
         }
-
-        else if ($(this).hasClass('h50')) {
-            ratio = 2;
-            h = parentWidth / ratio;
-        }
-
-        else if ($(this).hasClass('h33')) {
-            ratio = 3;
-            h = parentWidth / ratio;
-        }
-
-        else if ($(this).hasClass('h25')) {
-            ratio = 4;
-            h = parentWidth / ratio;
-        }
-
         else {
             return;
         }
-        
-				$(this).css('min-height', h) + margin;
+
+        $(this).addClass(wclass);
+		$(this).css('min-height', h);
         $(this).children('.inner').css('min-height', h - marginTop - marginBottom - paddingTop - paddingBottom  - borderTopWidth - borderBottomWidth);
 
     });
