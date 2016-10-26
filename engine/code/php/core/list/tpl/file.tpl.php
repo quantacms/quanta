367709f9bd280c70de6271a4de0740f3bf89e781
aldotripiciano <aldo.tripiciano@gmail.com>
2016-10-26 16:55:55 +0200

    PHP

--- engine/code/php/core/list/tpl/file.tpl.php
+++ engine/code/php/core/list/tpl/file.tpl.php
@@ -1,1 +1,5 @@
-<li class="list-item list-item-file list-item-file-admin file-[EXT]"><a class="file-link" href="[URL]">[TITLE]</a></li>
+<li class="list-item list-item-file list-item-file-admin file-[FILEATTRIBUTE|name=type|node={LISTNODE}:{LISTITEM}]">
+  <a class="file-link" href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
+    [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
+  </a>
+</li>
--- engine/code/php/core/list/tpl/simple.tpl.php
+++
@@ -1,1 +0,0 @@
-[NODE:name]:[NODE:title]
