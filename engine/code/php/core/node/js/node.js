ba634babdc29c4f2ec99502360c62660d014c457
aldotripiciano <aldo.tripiciano@gmail.com>
2016-11-04 16:06:36 +0100

    Engine

--- engine/code/php/core/node/js/node.js
+++ engine/code/php/core/node/js/node.js
@@ -4,13 +4,14 @@ $(document).bind('shadow_node_edit_submit', function(ev) {
    $('#edit_content').html(edata);
 });
 
-$(document).ready(function() {
+$(document).bind('refresh', function() {
+
     $('.delete-link').on('click', function() {
-        openShadow({ module : 'node', context: 'node_delete', widget: 'single'});
+        openShadow({ module : 'node', context: 'node_delete', widget: 'single', node: $(this).attr('rel')});
     });
 
     $('.edit-link').bind('click', function() {
-        openShadow({ module : 'node', context: 'node_edit', widget: 'tabs'});
+        openShadow({ module : 'node', context: 'node_edit', widget: 'tabs', node: $(this).attr('rel')});
     });
 
     $('.add-link').each(function() {
@@ -24,6 +25,14 @@ $(document).ready(function() {
         });
     });
 
+    $('.node-item').on('mouseenter', function() {
+        $(this).parent().css('opacity', '0.5');
+        $(this).children('.node-item-actions').show();
+    });
 
+    $('.node-item').on('mouseleave', function() {
+        $(this).parent().css('opacity', '1');
+        $(this).children('.node-item-actions').hide();
+    });
 
 });
