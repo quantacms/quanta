c581aa9181f1d7159f39d8fd5d2310bd34ced383
aldotripiciano <aldo.tripiciano@gmail.com>
2016-12-04 18:00:50 +0100

    Engine

---
+++ engine/code/php/core/media/tpl/playlist.tpl.php
@@ -0,0 +1,4 @@
+<div class="">
+  <div class="audio-title">[FILEATTRIBUTE|name=name:{LISTITEM}]</div>
+  <div class="audio-player">[AUDIO:{LISTITEM}]</div>
+</div>
--- engine/code/php/core/wysiwyg/froala/js/froala-quanta.js
+++ engine/code/php/core/wysiwyg/froala/js/froala-quanta.js
@@ -3,9 +3,4 @@ $(document).bind('shadow_node_edit',function(ev) { froala(); });
 
 function froala() {
     $('#edit_content').froalaEditor();
-    $('div[style*="z-index: 9999"]').hide();
 }
-
-$(document).bind('keyup', function() {
-    $('div[style*="z-index: 9999"]').hide();
-});
