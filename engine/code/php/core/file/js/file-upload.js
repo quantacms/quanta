01d7b9ceb494eadb5b6f27f100be412eb49804cd
aldotripiciano <aldo.tripiciano@gmail.com>
2016-12-05 17:59:33 +0100

    Engine almost stable

--- engine/code/php/core/file/js/file-upload.js
+++ engine/code/php/core/file/js/file-upload.js
@@ -45,7 +45,6 @@ $(function(){
 
             });
 
-            e.preventDefault();
             // Automatically upload the file once it is added to the queue
             var jqXHR = data.submit();
         },
