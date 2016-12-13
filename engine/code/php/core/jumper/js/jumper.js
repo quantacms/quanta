6033c6cf5e372c8b7d6474423cb4c381ca7efacd
aldotripiciano <aldo.tripiciano@gmail.com>
2016-12-13 18:03:24 +0100

    CMS new version

--- engine/code/php/core/jumper/js/jumper.js
+++ engine/code/php/core/jumper/js/jumper.js
@@ -2,18 +2,18 @@ var refreshJumpers = function() {
     $('.jumper').each(function() {
         $(this).bind('change', function() {
             var rel = $(this).attr('rel');
-
+            var tpl = $(this).attr('tpl');
             if (rel == '_self') {
                 top.location.href = '/' + $(this).val();
             }
             else if ($(this).val() != '_empty') {
-                openAjax('/' + $(this).val(), rel, 'refreshJumpers');
+                openAjax('/' + $(this).val(), rel, 'refreshJumpers', tpl);
             }
         });
     });
 }
 
-$(document).ready(function() {
+$(document).bind('refresh', function() {
     refreshJumpers();
 });
 
