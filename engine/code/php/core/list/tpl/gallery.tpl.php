be732ee99b13cd41e13fc330d8dc4bac4f20c5e4
aldotripiciano <aldo.tripiciano@gmail.com>
2016-12-15 12:10:38 +0100

    Gallery and stuff

--- engine/code/php/core/list/tpl/gallery.tpl.php
+++ engine/code/php/core/list/tpl/gallery.tpl.php
@@ -1,1 +1,8 @@
-<a class="gallery-thumb"  href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]"><div class="thumb-container"><img src="/engine/code/php/thumb.php?file=[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]&maxw=600&maxh=250" /></div><div class="gallery-item-title">[FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]</div></a>
+<a class="gallery-thumb"  href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
+  <div class="thumb-container">
+    [IMGTHUMB:{LISTNODE}/{LISTITEM}]
+  </div>
+  <div class="gallery-item-title">
+    [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
+  </div>
+</a>
