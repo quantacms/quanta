1eb97025a67601f749317565e913f42b70602091
Aldo Tripiciano <aldo@luv.it>
2016-12-17 20:14:38 +0000

    Engine

--- engine/code/php/core/tooltip/js/tooltip.js
+++ engine/code/php/core/tooltip/js/tooltip.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
 $(document).bind('refresh', function() {
     $('[tooltip]').on('mouseover', function(e) {
 
+
         $('#tooltip').html($(this).attr('tooltip')).show();
         var tooltip = $('#tooltip');
-        $(document).off('mousemove').on('mousemove', function(e) {
-            var tipLeft = e.pageX > ($(window).width() / 2) ? (e.pageX - tooltip.innerWidth()) : (e.pageX + 10);
-            var tipTop = e.pageY > ($(window).height() / 2) ? (e.pageY - + tooltip.innerHeight()) : (e.pageY + 10);
+				tooltip.css('color', $(this).css('color'));
+				$(document).off('mousemove').on('mousemove', function(e) {
+            var tipLeft = (e.pageX > ($(window).width() / 2)) ? (e.pageX - tooltip.innerWidth()) : (e.pageX + 10);
+            var tipTop = (e.pageY > ($(window).height() / 2)) ? (e.pageY - tooltip.innerHeight()) : (e.pageY + 10);
             $('#tooltip').css({
                 left:  tipLeft,
                 top:   tipTop
