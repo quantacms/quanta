ef07cfea31d5a453024a298bbd0c3a63fc9b44a1
aldotripiciano <aldo.tripiciano@gmail.com>
2017-01-23 13:06:27 +0100

    Engine

---
+++ engine/code/php/core/flickity/tpl/flickity-carousel.tpl.php
@@ -0,0 +1,11 @@
+<div class="slide" href="[URL:[LISTITEM]]">
+  <div class="slide-content">
+    <div class="vertical-aligner">
+      <div class="vertical-aligner-inner">
+        <h3 class="slide-title">[ATTRIBUTE|name=title:[LISTITEM]]</h3>
+        <div class="slide-text">[ATTRIBUTE|name=teaser:[LISTITEM]]</div>
+      </div>
+    </div>
+  </div>
+  <div class="slide-img" style="">[THUMBNAIL:[LISTITEM]]</div>
+</div>
