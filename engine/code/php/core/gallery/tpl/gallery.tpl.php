1cd1422462738e58c733d178ad6deaa7fb92b90e
dario <dario.capocci@gmail.com>
2017-03-14 17:19:13 +0100

    spostato gallery.tpl.php nel sul modulo

---
+++ engine/code/php/core/gallery/tpl/gallery.tpl.php
@@ -0,0 +1,8 @@
+<a class="gallery-thumb"  href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
+  <div class="thumb-container">
+    [IMGTHUMB|node={LISTNODE}:{LISTITEM}]
+  </div>
+  <div class="gallery-item-title">
+    [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
+  </div>
+</a>
