bb8da21df3966cda5548815c707e4f89b1b4c3cf
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2017-10-12 15:12:32 +0200

    New language stuff etc.

--- engine/code/php/core/shadow/js/shadow.js
+++ engine/code/php/core/shadow/js/shadow.js
@@ -1,14 +1,14 @@
 var shadow;
 
-$(document).bind('refresh', function() {
-    $('#shadow-inside, #shadow-image').off('click').on('click', function () {
-        closeShadow();
-    });
+$(document).bind('refresh', function () {
+  $('#shadow-inside, #shadow-image').off('click').on('click', function () {
+    closeShadow();
+  });
 
 });
 
 function closeShadow() {
-    $('#shadow-outside, .shadow-element').hide();
+  $('#shadow-outside, .shadow-element').hide();
 }
 
 /**
@@ -16,62 +16,63 @@ function closeShadow() {
  * @param shadow
  */
 function openShadow(shadowData) {
-    shadow = shadowData;
-    if (shadow.widget == undefined) {
-        shadow.widget = 'tabs';
+  shadow = shadowData;
+  if (shadow.widget == undefined) {
+    shadow.widget = 'tabs';
+  }
+
+  var shadowPath = '/' + ((shadow.node != undefined) ? (shadow.node + '/') : '');
+  shadowPath += '?shadow=' + JSON.stringify(shadow);
+  if (shadow.language != undefined) {
+    shadowPath += '&lang=' + shadow.language;
+  }
+  $('#shadow-item').html('').attr('data-rel', shadow.context).load(shadowPath, function () {
+    if (shadow.callback != undefined) {
+      shadow.callback();
     }
 
-    var shadowLanguage = (shadow.language != undefined) ? (shadow.language) : 'fallback';
-    var shadowPath = '/' + ((shadow.node != undefined) ? (shadow.node + '/') : '') + '?lang=' + shadowLanguage + '&shadow=' + JSON.stringify(shadow);
+    $(document).trigger('refresh');
+    // Include attached scripts if present.
+    if (shadow.attach != undefined) {
+      for (i = 0; i < shadow.attach.length; i++) {
+        $.getScript(shadow.attach[i]);
+      }
+    }
 
-    $('#shadow-item').html('').attr('data-rel', shadow.context).load(shadowPath, function () {
-            if (shadow.callback != undefined) {
-                shadow.callback();
-            }
-
-            $(document).trigger('refresh');
-
-            // Include attached scripts if present.
-            if (shadow.attach != undefined) {
-                for (i = 0; i < shadow.attach.length; i++) {
-                    $.getScript(shadow.attach[i]);
-                }
-            }
-
-            $('.shadow-title').find('a').on('click', function () {
-                if (!($(this).parent().hasClass('enabled'))) {
-                    $('.enabled').removeClass('enabled');
-                    $(this).parent().addClass('enabled');
-                    $('#shadow-content-' + $(this).attr('data-rel')).addClass('enabled');
-                }
-                return false;
-            });
-            $(document).trigger('shadow_open');
-            $(document).trigger('shadow_' + shadow.context);
-        });
-    $('#shadow-outside').fadeIn('slow');
+    $('.shadow-title').find('a').on('click', function () {
+      if (!($(this).parent().hasClass('enabled'))) {
+        $('.enabled').removeClass('enabled');
+        $(this).parent().addClass('enabled');
+        $('#shadow-content-' + $(this).attr('data-rel')).addClass('enabled');
+      }
+      return false;
+    });
+    $(document).trigger('shadow_open');
+    $(document).trigger('shadow_' + shadow.context);
+  });
+  $('#shadow-outside').fadeIn('slow');
 }
 
 /**
  * Submit a shadow form.
  */
 function submitShadow() {
-    $(document).trigger('shadow_' + shadow.context + '_submit');
-    var form_items = {};
-    $('#shadow-outside').find('input, textarea, select').each(function () {
-        var item_name = $(this).attr('name');
-        if (form_items[item_name] == undefined) {
-            form_items[item_name] = [];
-        }
-        if ($(this).attr('type') == 'checkbox') {
-            form_items[item_name].push($(this).is(':checked') ? $(this).val() : '');
-        } else {
-            form_items[item_name].push($(this).val());
-        }
-    });
-    var formData = JSON.stringify(form_items);
+  $(document).trigger('shadow_' + shadow.context + '_submit');
+  var form_items = {};
+  $('#shadow-outside').find('input, textarea, select').each(function () {
+    var item_name = $(this).attr('name');
+    if (form_items[item_name] == undefined) {
+      form_items[item_name] = [];
+    }
+    if ($(this).attr('type') == 'checkbox') {
+      form_items[item_name].push($(this).is(':checked') ? $(this).val() : '');
+    } else {
+      form_items[item_name].push($(this).val());
+    }
+  });
+  var formData = JSON.stringify(form_items);
 
-    action(formData);
+  action(formData);
 }
 
 
