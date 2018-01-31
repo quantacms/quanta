609d63634b096da3c561a95f49f08146c9515a00
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2018-01-31 10:33:10 +0100

    User fixes and others

--- engine/code/php/core/tooltip/js/tooltip.js
+++ engine/code/php/core/tooltip/js/tooltip.js
@@ -1,25 +1,26 @@
-$(document).ready(function() {
-  $('body').append('<div id="tooltip"></div>');				
+$(document).ready(function () {
+  $('body').append('<div id="tooltip"></div>');
 });
 
-$(document).bind('refresh', function() {
-    $('[data-tooltip]').on('mouseover', function(e) {
-
-
-        $('#tooltip').html($(this).attr('data-tooltip')).show();
-        var tooltip = $('#tooltip');
-				tooltip.css('color', $(this).css('color'));
-				$(document).off('mousemove').on('mousemove', function(e) {
-            var tipLeft = (e.pageX > ($(window).width() / 2)) ? (e.pageX - tooltip.innerWidth()) : (e.pageX + 10);
-            var tipTop = (e.pageY > ($(window).height() / 2)) ? (e.pageY - tooltip.innerHeight()) : (e.pageY + 10);
-            $('#tooltip').css({
-                left:  tipLeft,
-                top:   tipTop
-            });
-        });
+$(document).bind('refresh', function () {
+  var dataTooltip = $(['data-tooltip']);
+  // Create a mouseover effect to display tooltips.
+  dataTooltip.on('mouseover', function () {
+    var tooltip = $('#tooltip');
+    // Generate the tooltip's html.
+    tooltip.html($(this).attr('data-tooltip')).show();
+    tooltip.css('color', $(this).css('color'));
+    $(document).off('mousemove').on('mousemove', function (e) {
+      var tipLeft = (e.pageX > ($(window).width() / 2)) ? (e.pageX - tooltip.innerWidth()) : (e.pageX + 10);
+      var tipTop = (e.pageY > ($(window).height() / 2)) ? (e.pageY - tooltip.innerHeight()) : (e.pageY + 10);
+      tooltip.css({
+        left: tipLeft,
+        top: tipTop
+      });
     });
-    $('[data-tooltip]').on('mouseout', function(e) {
-        $('#tooltip').unbind();
-        $('#tooltip').hide();
-    });
+  });
+  dataTooltip.on('mouseout', function () {
+    tooltip.unbind();
+    tooltip.hide();
+  });
 });
