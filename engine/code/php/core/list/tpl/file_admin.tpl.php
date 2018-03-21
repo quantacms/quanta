5f60f586ed6bac629903b770c277b180dd29903b
Dinamico <admin@dinamicoweb.it>
2018-03-21 18:11:54 +0000

    added sort icon in file manager

--- engine/code/php/core/list/tpl/file_admin.tpl.php
+++ engine/code/php/core/list/tpl/file_admin.tpl.php
@@ -1,5 +1,11 @@
+<span class="sort-handle"></span>
 <span class="file-link-item file-[FILEATTRIBUTE|name=type|node={LISTNODE}:{LISTITEM}]">
+  <span class="file-preview">file preview</span>  
   <a class="file-link" data-node="{LISTNODE}" href="[FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]">
     [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
   </a>
 </span>
+<!-- TODO: add up and down sorting
+<button class="sort-up">up</button>
+<button class="sort-down">down</button>
+-->
