5647e3b97cec6db209094e07270c3638f71a9843
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2018-04-27 17:40:33 +0200

    File preview system

--- engine/code/php/core/list/tpl/file_admin.tpl.php
+++ engine/code/php/core/list/tpl/file_admin.tpl.php
@@ -1,5 +1,5 @@
 <span class="sort-handle"></span>
-<span class="file-link-item file-[FILEATTRIBUTE|name=type|node={LISTNODE}:{LISTITEM}]">
+<span class="file-link-item">
   <span class="file-preview">file preview</span>  
   <a class="file-link" data-node="{LISTNODE}" href="[FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]">
     [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
