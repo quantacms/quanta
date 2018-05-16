06002552ff2b1e507dabc94c38b744de5aeb9f25
Dinamico <admin@dinamicoweb.it>
2018-05-16 14:45:33 +0000

    target _blank in filelist

--- engine/code/php/core/list/tpl/file.tpl.php
+++ engine/code/php/core/list/tpl/file.tpl.php
@@ -1,5 +1,5 @@
 <span class="file-link-item file-[FILEATTRIBUTE|name=type|node={LISTNODE}:{LISTITEM}]">
-  <a class="file-link" data-node="{LISTNODE}" href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
+  <a class="file-link" data-node="{LISTNODE}" target="_blank" href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
     [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
   </a>
 </span>
