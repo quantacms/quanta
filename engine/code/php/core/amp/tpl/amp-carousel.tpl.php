48a7bc0ad0e6ea1b0907c4f58ce40e6729df4370
Dinamico <admin@dinamicoweb.it>
2018-07-01 00:21:06 +0000

    temp disabled JS in BOX. Added amp-carousel in amp module

---
+++ engine/code/php/core/amp/tpl/amp-carousel.tpl.php
@@ -0,0 +1,14 @@
+<div class="slide">
+  <amp-img src="[IMGTHUMBURL|node=[LISTITEM]|1000x0:{ATTRIBUTE|name=thumbnail:[LISTITEM]}]"
+    width="400"
+    height="225"
+    layout="responsive"
+    alt="[ATTRIBUTE|name=title:[LISTITEM]]">
+  </amp-img>
+  <amp-fit-text
+    layout="fixed-height"
+    height="50"
+    min-font-size="12"
+    max-font-size="24">[LINK:[LISTITEM]]
+  </amp-fit-text>
+</div>
---
+++ engine/code/php/core/amp/tpl/amp-file-carousel.tpl.php
@@ -0,0 +1,14 @@
+<div class="slide">
+  <amp-img src="[IMGTHUMBURL|node={LISTNODE}:{LISTITEM}]"
+    width="400"
+    height="225"
+    layout="responsive"
+    alt="{LISTITEM}">
+  </amp-img>
+  <amp-fit-text
+    layout="fixed-height"
+    height="50"
+    min-font-size="12"
+    max-font-size="24">{LISTITEM}
+  </amp-fit-text>
+</div>
