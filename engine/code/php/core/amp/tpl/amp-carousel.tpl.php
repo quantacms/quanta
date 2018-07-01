c2cc70e7cd011d41133e9b512b9cca1e0dd12651
Dinamico <admin@dinamicoweb.it>
2018-07-01 10:49:08 +0000

    centralized css on amp module

--- engine/code/php/core/amp/tpl/amp-carousel.tpl.php
+++ engine/code/php/core/amp/tpl/amp-carousel.tpl.php
@@ -1,14 +1,12 @@
 <div class="slide">
   <amp-img src="[IMGTHUMBURL|node=[LISTITEM]|1000x0:{ATTRIBUTE|name=thumbnail:[LISTITEM]}]"
+    class="cover"
     width="400"
     height="225"
-    layout="responsive"
-    alt="[ATTRIBUTE|name=title:[LISTITEM]]">
+    layout="fill"
+    alt="[TITLE:[LISTITEM]]">
+    <div class="caption">
+      [LINK:[LISTITEM]]
+    </div>    
   </amp-img>
-  <amp-fit-text
-    layout="fixed-height"
-    height="50"
-    min-font-size="12"
-    max-font-size="24">[LINK:[LISTITEM]]
-  </amp-fit-text>
 </div>
