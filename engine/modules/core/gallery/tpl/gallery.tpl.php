905eba2853ea1555c400f2096ca69283bf9055c3
Aldo Tripiciano <aldo@luv.it>
2018-07-13 09:15:07 +0000

    Quick fix

--- engine/modules/core/gallery/tpl/gallery.tpl.php
+++ engine/modules/core/gallery/tpl/gallery.tpl.php
@@ -1,6 +1,6 @@
 <a class="gallery-thumb"  href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
   <div class="thumb-container">
-    [IMGTHUMB|node={LISTNODE}:{LISTITEM}]
+    [IMGTHUMB|150x150|node={LISTNODE}:{LISTITEM}]
   </div>
   <div class="gallery-item-title">
     [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
