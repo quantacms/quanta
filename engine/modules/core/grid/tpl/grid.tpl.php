6d3df4f525eefaa7a25551fdec736c3f80f1980b
Dinamico <admin@dinamicoweb.it>
2018-09-20 12:36:31 +0000

    working on GRID, Flickity theming

---
+++ engine/modules/core/grid/tpl/grid.tpl.php
@@ -0,0 +1,8 @@
+<div class="grid grid-container slide">
+  <div class="grid-start-3 grid-span-8 align-self-center text-md-left slide-content">
+    <h1 class="slide-title">[TITLE:[LISTITEM]]</h1>
+    <div class="slide-text">[TEASER:[LISTITEM]]</div>
+    <a class="slide-link" href="/[LISTITEM]">View more</a>
+  </div>
+  <div class="slide-img">[IMGTHUMB|node=[LISTITEM]|1000x0:{ATTRIBUTE|name=thumbnail:[LISTITEM]}]</div>
+</div>
