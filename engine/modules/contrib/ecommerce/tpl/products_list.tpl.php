f83dff5de038b557ffbb407c0ba9064abf5bc9f9
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2018-10-08 16:48:08 +0200

    Coding standards and installer fix

--- engine/modules/contrib/ecommerce/tpl/products_list.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="product">
-    <div class="product-thumb">[THUMBNAIL:[LISTITEM]]</div>
-    <div class="product-info">
-    <div class="product-name"><h3>[LINK:[LISTITEM]]</h3></div>
-    <div class="product-price">&euro; [ATTRIBUTE|name=price:[LISTITEM]]</div>
-    <div class="product-availability product-[ATTRIBUTE|name=availability:[LISTITEM]]">[ATTRIBUTE|name=availability:[LISTITEM]]</div>
-  </div>
-</div>
