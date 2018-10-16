c42903b085dbaf7d465d89056bf9ed9a24d40f11
Dinamico <admin@dinamicoweb.it>
2018-10-16 13:23:28 +0000

    removed attributes in tags when not present, div to article

--- engine/modules/core/grid/tpl/grid-carousel.tpl.php
+++ engine/modules/core/grid/tpl/grid-carousel.tpl.php
@@ -1,8 +1,8 @@
-<div class="grid grid-container slide">
+<article class="grid grid-container slide">
   <div class="grid-start-md-2 grid-span-md-10 grid-start-lg-3 grid-span-lg-8 p-x align-self-center text-md-left slide-content">
     <h1 class="slide-title">[TITLE:[LISTITEM]]</h1>
     <div class="slide-text">[TEASER:[LISTITEM]]</div>
     <a class="slide-link" href="/[LISTITEM]">View more</a>
   </div>
   <div class="slide-img">[IMGTHUMB|node=[LISTITEM]|1000x0:{ATTRIBUTE|name=thumbnail:[LISTITEM]}]</div>
-</div>
+</article>
