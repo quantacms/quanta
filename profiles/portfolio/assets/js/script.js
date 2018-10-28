14a9764c39addb7a637388782ac345e106466720
Onwuka Gideon <dongidomed@gmail.com>
2018-10-28 21:18:19 +0000

    updated HTMl tag to use qtags

--- profiles/portfolio/assets/js/script.js
+++ profiles/portfolio/assets/js/script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var last_known_scroll_position = 0;
-    const nav = document.getElementById("nav");
+    const menu = document.getElementById("menu");
     
     var ticking = false;
     
     function doSomething(scroll_pos) {
-      if ( scroll_pos > 140 && nav) {
-        nav.classList.add("mobile-drop");
+      if ( scroll_pos > 140 && menu) {
+        menu.classList.add("fixed-menu");
       } else {
-        nav.classList.remove("mobile-drop");
+        menu.classList.remove("fixed-menu");
       }
     }
     
