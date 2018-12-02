b12deb3c8b1869602f305e466f4b448bd129257e
Aldo Tripiciano <aldo.tripiciano@gmail.com>
2018-12-02 10:32:29 +0100

    Update autoload.php

--- src/autoload.php
+++ src/autoload.php
@@ -6,29 +6,3 @@
  *
  * Ideas and contributions welcome at https://www.github.com/quantacms/quanta
  */
-
-/**
- * Iterate all the modules and load them.
- */
-foreach (new DirectoryIterator('modules') as $module) {
-  if (!($module->isDot())) {
-    print $module->getFilename();
-    require_once('modules/' . $module . '/' . $module . '.module');
-  }
-}
-
-print_r($modules);
-die();
-
-// Include the Environment module.
-require_once('modules/environment/environment.module');
-
-// Include the Cache module.
-require_once('modules/cache/cache.module');
-
-/**
- * Load core modules.
- */
-  $core_modules = $this->scanDirectory($this->dir('modules_core') , array('type' => DIR_MODULES));
-  $this->setModules('core', $core_modules);
-
