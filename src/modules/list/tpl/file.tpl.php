c10e123871da5c57d182dee6dc60ecc36b60f739
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2018-12-22 22:07:13 +0100

    Fixing FILEATTRIBUTE -> FILE_ATTRIBUTE

--- src/modules/list/tpl/file.tpl.php
+++ src/modules/list/tpl/file.tpl.php
@@ -1,5 +1,5 @@
-<span class="file-link-item file-[FILEATTRIBUTE|name=type|node={LISTNODE}:{LISTITEM}]">
-  <a class="file-link" data-node="{LISTNODE}" target="_blank" href="[FILEATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
-    [FILEATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
+<span class="file-link-item file-[FILE_ATTRIBUTE|name=type|node={LISTNODE}:{LISTITEM}]">
+  <a class="file-link" data-node="{LISTNODE}" target="_blank" href="[FILE_ATTRIBUTE|name=path|node={LISTNODE}:{LISTITEM}]">
+    [FILE_ATTRIBUTE|name=name|node={LISTNODE}:{LISTITEM}]
   </a>
 </span>
