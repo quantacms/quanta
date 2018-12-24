52c8a8802afdade383511f5c54d6b8a5ce630406
Dinamico <admin@dinamicoweb.it>
2018-12-24 08:52:20 +0000

    from LIST to LIST_NODES

--- src/modules/media/tpl/album.tpl.php
+++ src/modules/media/tpl/album.tpl.php
@@ -1,4 +1,4 @@
 <div class="album">
   <h2 class="album-title">[LINK:[LISTITEM]]</h2>
-  <div class="audio-player">[LIST:[LISTITEM]]</div>
+  <div class="audio-player">[LIST_NODES:[LISTITEM]]</div>
 </div>
