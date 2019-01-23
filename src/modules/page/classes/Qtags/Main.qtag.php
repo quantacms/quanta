5bd28e872d2451d73db2bab8e7187a1e0de0c162
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2019-01-23 15:53:58 +0100

    Updating HTML tags

--- src/modules/page/classes/Qtags/Main.qtag.php
+++ src/modules/page/classes/Qtags/Main.qtag.php
@@ -4,14 +4,5 @@ namespace Quanta\Qtags;
  * Render a page's main content (<main> tag).
  */
 class Main extends Content {
-  /**
-   * @return string
-   *   The rendered Qtag.
-   */
-  public function render() {
-    $this->attributes['grid_html_tag'] = 'main';
-    $grid_attr = isset($this->attributes['grid']) ? $this->attributes['grid'] : '';
-    $this->attributes['grid'] = 'grid ' . $grid_attr;
-    return parent::render();
-  }
+  public $html_tag = 'main';
 }
