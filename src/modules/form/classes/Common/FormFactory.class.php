f483e1a520f7204dee406ae0a60243f2a1a2739c
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2019-03-19 10:06:23 +0100

    Full refactoring of Form System

--- src/modules/form/classes/Common/FormFactory.class.php
+++ src/modules/form/classes/Common/FormFactory.class.php
@@ -15,11 +15,11 @@ class FormFactory {
    * @param string $form_id
    *   The form ID.
    *
-   * @return Form
+   * @return FormState
    *   The created form.
    */
-  public static function createForm($env, $form_id) {
-    $form = new Form($env, $form_id, array());
+  public static function createFormState($env, $form_id, $type = NULL) {
+    $form = new FormState($env, $form_id, $type);
     return $form;
   }
 
@@ -32,17 +32,16 @@ class FormFactory {
    * @param string $form_id
    *   The id of the form.
    *
-   * @return Form
-   *   The retrieved form.
+   * @return FormState
+   *   The retrieved form state.
    */
-  public static function getForm($env, $form_id = FORM_PAGE_FORM) {
-    $form = $env->getData('form_' . $form_id);
-    if (empty($form)) {
-      $form = FormFactory::createForm($env, $form_id);
-      $env->setData('form_' . $form_id, $form);
+  public static function getFormState($env, $form_id = FORM_PAGE_FORM) {
+    $form_state = $env->getData('form_' . $form_id);
+    if (empty($form_state)) {
+      $form_state = FormFactory::createFormState($env, $form_id);
+      $env->setData('form_' . $form_id, $form_state);
     }
-
-    return $form;
+    return $form_state;
   }
 
   /**
@@ -54,79 +53,81 @@ class FormFactory {
    * @param array $input
    *   The input item as it comes from the qtag.
    *
-   * @param Form $form
+   * @param FormState $form
    *   The form.
    *
-   * @return FormItem
+   * @return \Quanta\Qtags\FormItem
    *   The constructed form item.
    */
-  public static function createInputItem($env, $input, $form) {
+  public static function createInputItem($env, $input, &$form) {
+
     if (empty($input['type'])) {
       $input['type'] = 'string';
     }
     switch($input['type']) {
 
       case 'file':
-        $formitem = new FormItemFile($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemFile($env, $input, $form);
         break;
 
       case 'text':
-        $formitem = new FormItemText($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemText($env, $input, $form);
         break;
 
       case 'hidden':
-        $formitem = new FormItemHidden($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemHidden($env, $input, $form);
         break;
 
       case 'select':
-        $formitem = new FormItemSelect($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemSelect($env, $input, $form);
         break;
 
       case 'checkbox':
-        $formitem = new FormItemCheckbox($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemCheckbox($env, $input, $form);
         break;
 
       case 'date':
-        $formitem = new FormItemDate($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemDate($env, $input, $form);
         break;
 
       case 'time':
-        $formitem = new FormItemTime($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemTime($env, $input, $form);
         break;
 
       case 'number':
-        $formitem = new FormItemNumber($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemNumber($env, $input, $form);
         break;
 
       case 'email':
-        $formitem = new FormItemEmail($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemEmail($env, $input, $form);
         break;
 
       case 'url':
-        $formitem = new FormItemUrl($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemUrl($env, $input, $form);
         break;
 
       case 'password':
-        $formitem = new FormItemPassword($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemPassword($env, $input, $form);
         break;
 
       case 'submit':
-        $formitem = new FormItemSubmit($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemSubmit($env, $input, $form);
         break;
 
       case 'autocomplete':
-        $formitem = new FormItemAutocomplete($env, $input);
+        $formitem = new \Quanta\Qtags\FormItemAutocomplete($env, $input, $form);
         break;
 
       case 'string':
       default:
-      // TODO: use a hook to eventually get custom formitem items from other modules.
-      $formitem = new FormItemString($env, $input);
-        break;
-
+        // TODO: use a hook to eventually get custom formitem items from other modules.
+        $formitem = new \Quanta\Qtags\FormItemString($env, $input, $form);
+      break;
     }
 
+    // Add the form item to the form state.
     $form->addItem($formitem->getName(), $formitem);
+
     return $formitem;
 
   }
--- src/modules/form/classes/Qtags/Input.qtag.php
+++ src/modules/form/classes/Qtags/Input.qtag.php
@@ -4,7 +4,10 @@ namespace Quanta\Qtags;
 /**
  * Prepares an input item of a form for rendering.
  */
-class Input extends Qtag {
+class Input extends HtmlTag {
+  protected $form_state;
+  protected $form_item;
+
   /**
    * Render the Qtag.
    *
@@ -12,18 +15,131 @@ class Input extends Qtag {
    *   The rendered Qtag.
    */
   public function render() {
-    $form = \Quanta\Common\FormFactory::getForm($this->env, $this->getTarget());
-    \Quanta\Common\FormFactory::createInputItem($this->env, $this->attributes, $form);
-    $rendered = '';
+    $this->form_state = \Quanta\Common\FormFactory::getFormState($this->env, $this->getTarget());
+    $this->form_item = \Quanta\Common\FormFactory::createInputItem($this->env, $this->attributes, $this->form_state);
 
-    if (!(empty($this->attributes['name'])) && !(empty($form->getItem($this->attributes['name'])))) {
-      $input = $form->getItem($this->attributes['name']);
-      if ($input->isFirst()) {
-        $rendered = $form->renderFormOpen() . $rendered;
-      }
-      $rendered .= ($form->isValidated()) ? '' : $input->renderFormItem();
+    if ($this->form_state->isSubmitted()) {
+      $this->form_item->validate();
+
+      $vars = array('form_item' => &$this->form_item);
+      // TODO: add other useful smart hooks.
+      $this->env->hook('form_item_validate', $vars);
+      $this->env->hook($this->getFormState()->getId() . '_form_item_validate', $vars);
+
     }
 
-    return $rendered;
+    $values = $this->form_item->getValue();
+    $i = 0;
+    // Load and render all existing values...
+    // @TODO refactor!.
+    foreach ($values as $value) {
+      // Set the current value of the form item.
+      $this->form_item->setCurrentValue($value);
+      if ($i > 0) {
+        $this->setId($this->getId() . '_' . $i);
+      }
+      // Render the form item using its custom render function.
+      $rend = $this->form_item->render();
+      // If the item is multiple, add a wrapper for this instance.
+      $this->html_body .= ($this->form_item->isMultiple()) ? ('<div class="form-item-multiple-wrapper">' . $rend . '</div>') :  $rend;
+      $i++;
+    }
+
+    // Name attribute is mandatory to render an input.
+    if (!(empty($this->getAttribute('name')))) {
+      // Add a label to the form item.
+      if (!empty($this->form_item->getLabel())) {
+        $label_attributes = array(
+          'label_position' => $this->form_item->getLabelPosition(),
+          'label_text' => $this->form_item->getLabel(),
+          'required' => $this->form_item->isRequired(),
+          );
+        $label = new Label($this->env, $label_attributes, $this->form_item->getId());
+        if ($this->form_item->getLabelPosition() == Label::LABEL_ASIDE) {
+          $this->html_body = $this->html_body . $label->render();
+        }
+        elseif ($this->form_item->getLabelPosition() == Label::LABEL_ON_TOP) {
+          $this->html_body = $label->render() . $this->html_body;
+        }
+      }
+
+      $this->addClass('form-item-wrapper');
+      if ($this->hasValidationErrors()) {
+        $error_attr = array();
+        $error = new ValidationError($this->env, $error_attr, $this->getValidationErrors());
+        $this->html_body = $error . $this->html_body;
+        $this->addClass('has-validation-errors');
+      }
+
+      return parent::render();
+    }
   }
+
+  /**
+   * Get all the validation errors of the submitted form item.
+   *
+   * @return mixed
+   *   The validation errors of the submitted form item, if any.
+   */
+  public function getValidationErrors() {
+    $errors = $this->getFormState()->getData('validation_errors');
+    return !empty($errors[$this->form_item->getName()]) ? $errors[$this->form_item->getName()] : NULL;
+  }
+
+  /**
+   * Check if this item is the last in the form.
+   * (Normally used to create the closing </form> tag.
+   *
+   * @return boolean
+   *   Returns true if the form item is the first one in the form.
+   */
+  public function isFirst() {
+    $items = $this->getFormState()->getItems();
+    $items = array_reverse($items);
+    $first = array_pop($items);
+    return ($this->form_item->getName() == $first->getName());
+  }
+
+
+  /**
+   * Check if this item is the last in the form.
+   * (Normally used to create the closing </form> tag.
+   *
+   * @return boolean
+   *   Returns true if the form item is the last one in the form.
+   */
+  public function isLast() {
+    // TODO: deprecated.
+    // With the new approach we have no way to determine if an input item is "LAST".
+  }
+
+  /**
+   * Setup the Form State containing this form item.
+   *
+   * @param \Quanta\Common\FormState
+   */
+  public function setFormState(&$form) {
+    $this->form_state = $form;
+  }
+
+  /**
+   * Returns the Form State containing this form item.
+   *
+   * @return \Quanta\Common\FormState
+   */
+  public function getFormState() {
+    return $this->form_state;
+  }
+
+  /**
+   * Check if a submitted form has validation errors.
+   *
+   * @return boolean
+   *   Returns true if the submitted form has validation errors.
+   */
+  public function hasValidationErrors() {
+    $has_validation_errors = !empty($this->getValidationErrors());
+    return $has_validation_errors;
+  }
+
 }
