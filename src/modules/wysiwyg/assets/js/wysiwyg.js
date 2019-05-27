8b7f79778f4ccab3d73bea8e4f62b13ad4e0d851
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2019-05-27 12:27:45 +0200

    Fixing bug in multiple form items handling

--- src/modules/wysiwyg/assets/js/wysiwyg.js
+++ src/modules/wysiwyg/assets/js/wysiwyg.js
@@ -1,28 +1,30 @@
 ClassicEditor.builtinPlugins.map( plugin => plugin.pluginName );
 
-ClassicEditor
-  .create( document.querySelector( '.wysiwyg' ), {
-    alignment: {
-      options: [ 'left', 'center', 'right', 'justify' ]
-    },
-    toolbar: [ 'heading', '|', 'bold', 'italic', 'link', '|', 'bulletedList', 'numberedList', 'alignment','blockQuote', '|', 'undo', 'redo', '|', 'alignment', 'insertTable' ],
-    heading: {
-      options: [
-        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+$('.wysiwyg').each(function (elm) {
 
-      ]
-    }
-  } )
-  .then( editor => {
+  ClassicEditor
+    .create( this, {
+      alignment: {
+        options: [ 'left', 'center', 'right', 'justify' ]
+      },
+      toolbar: [ 'heading', '|', 'bold', 'italic', 'link', '|', 'bulletedList', 'numberedList', 'alignment','blockQuote', '|', 'undo', 'redo', '|', 'alignment', 'insertTable' ],
+      heading: {
+        options: [
+          { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+          { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+          { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+          { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+
+        ]
+      }
+    } )
+    .then( editor => {
     console.log( editor );
-    editor.model.document.on( 'change:data', ( evt, data ) => {
-      editor.updateSourceElement();
-    console.log( data );
-    });
-  })
-  .catch(error => {console.log( error );});
+  editor.model.document.on( 'change:data', ( evt, data ) => {
+    editor.updateSourceElement();
+});
+})
+.catch(error => {console.log( error );});
+});
 
 
