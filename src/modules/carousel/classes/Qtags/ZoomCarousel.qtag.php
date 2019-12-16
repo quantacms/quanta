5fedefa7a6b4f7822926634bb53b6a5108e22974
Aldo Tripiciano <Aldo.TRIPICIANO@ext.ec.europa.eu>
2019-12-16 11:32:55 +0100

    Zoom

--- src/modules/carousel/classes/Qtags/ZoomCarousel.qtag.php
+++ src/modules/carousel/classes/Qtags/ZoomCarousel.qtag.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Quanta\Qtags;
 
 /**
@@ -15,6 +16,48 @@ class ZoomCarousel extends FileCarousel {
   public function render() {
     $this->addClass('xzoom-thumbs');
     $this->attributes['tpl'] = 'zoom';
+
+    $available_params = array(
+      "position",
+      "mposition",
+      "rootOutput",
+      "Xoffset",
+      "Yoffset",
+      "fadeIn",
+      "fadeTrans",
+      "fadeOut",
+      "smoothZoomMove",
+      "smoothLensMove",
+      "smoothScale",
+      "defaultScale",
+      "scroll",
+      "tint",
+      "tintOpacity",
+      "lens",
+      "lensOpacity",
+      "lensShape",
+      "lensCollision",
+      "lensReverse",
+      "openOnSmall",
+      "zoomWidth",
+      "zoomHeight",
+      "sourceClass",
+      "loadingClass",
+      "lensClass",
+      "zoomClass",
+      "activeClass",
+      "hover",
+      "adaptive",
+      "adaptiveReverse",
+      "title",
+      "titleClass",
+      "bg");
+
+    foreach ($available_params as $param) {
+      if ($this->hasAttribute($param)) {
+        $this->html_params[$param] = $this->getAttribute($param);
+      }
+    }
     return parent::render();
   }
 }
