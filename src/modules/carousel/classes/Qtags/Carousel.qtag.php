f14ece4ad6c11a7fc31797f632a62a0ef7449104
Aldo Tripiciano <aldo@luv.it>
2019-12-16 11:08:21 +0000

    Bug fixes

--- src/modules/carousel/classes/Qtags/Carousel.qtag.php
+++ src/modules/carousel/classes/Qtags/Carousel.qtag.php
@@ -99,7 +99,7 @@ class Carousel extends HtmlTag {
           $main_img = new Img($this->env, $img_attr, $node->getThumbnail());
           $main_img->setAttribute('node', $node->getName());
 
-          $this->setAttribute('prefix', '<img id="main_image" class="xzoom" src="' . $node->getThumbnail() . '" xoriginal="' . $node->getThumbnail() . '');
+          $this->setAttribute('prefix', '<img id="main_image" class="xzoom" src="' . $node->getThumbnail() . '" xoriginal="' . $node->getThumbnail() . '" />');
           $page->addCSS($module_path . '/assets/css/xzoom.css');
           $page->addJS('/src/modules/carousel/assets/js/xzoom.js?test=' . rand(1, 100000), 'file');
           $page->addJS('$(".xzoom, .xzoom-gallery").xzoom({tint: "#333", Xoffset: 15});', 'inline');
