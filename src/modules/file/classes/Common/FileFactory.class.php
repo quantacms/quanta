6ce12caa94ce7a4229c2a8e3fba6989edf7f4a24
Aldo Tripiciano <aldo@luv.it>
2020-09-02 13:33:06 +0000

    Miscellaneous fixes

--- src/modules/file/classes/Common/FileFactory.class.php
+++ src/modules/file/classes/Common/FileFactory.class.php
@@ -29,9 +29,8 @@ class FileFactory {
           header('Pragma: public');
           header('Expires: 0');
           header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+          header('Content-Type: ' . mime_content_type($file));
           header('Cache-Control: private', false); // required for certain browsers
-          header('Content-Type: application/pdf');
-
           header('Content-Disposition: attachment; filename="'. basename($file) . '";');
           header('Content-Transfer-Encoding: binary');
           header('Content-Length: ' . filesize($file));
