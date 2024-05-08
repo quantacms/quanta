60468c9f34dd94a73f0bc5febfd71e33e7b37bba
Aldo Tripiciano <aldo.tripiciano@gmail.com>
2024-05-08 10:08:21 +0200

    body

--- src/modules/node/classes/Qtags/Body.qtag.php
+++ src/modules/node/classes/Qtags/Body.qtag.php
@@ -13,11 +13,8 @@ class Body extends Qtag {
   public function render() {
     $node = NodeFactory::loadOrCurrent($this->env, $this->getTarget());
     // TODO: breaks the HTML, but we definitely need a xss filter for this tag.
-    // $body = Api::filter_xss($node->getBody());
     $body = $node->getBody();
-    if (isset($attributes['editable'])) {
-      $body = NodeTemplate::wrap($this->env, $node, $body);
-    }
+    
     return $body;
   }
 }
