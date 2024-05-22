e9b0664a47d0909a7bd194133e92d49fcc39db20
MohamedTaha880 <mohamed.t@storexweb.com>
2024-05-22 04:25:12 -0400

    add replace attribute to text qtaq

--- src/modules/api/classes/Qtags/Text.qtag.php
+++ src/modules/api/classes/Qtags/Text.qtag.php
@@ -14,6 +14,19 @@ class Text extends Qtag {
    */
   public function render() {
     $tag = !isset($this->attributes['tag']) ? NULL : $this->attributes['tag'];
-    return \Quanta\Common\Localization::translatableText($this->env, $this->getTarget(), $tag);
+    $replace_string = $this->getAttribute('replace') ? $this->getAttribute('replace') : null;
+    $replace_arr = [];
+    if($replace_string){
+      // Explode the replace string by semicolon
+      $parts = explode(';', $replace_string);
+      // Iterate through the parts and create the associative array
+      foreach ($parts as $part) {
+        // Explode each part by '@' and directly check for exactly two parts
+        if (list($key, $value) = explode('@', $part, 2)) {
+            $replace_arr[$key] = $value;
+        }
+      }
+    }
+    return \Quanta\Common\Localization::translatableText($this->env, $this->getTarget(), $tag, null, $replace_arr);
   }
 }
