5dcea9bf940ccbf8a4e33cc05e2ecfa54be75d6f
MohamedTaha880 <mohamed.t@storexweb.com>
2024-06-03 07:48:48 -0400

    finish google docs permissions

--- src/modules/googleDrive/classes/Common/GoogleDrive.class.php
+++ src/modules/googleDrive/classes/Common/GoogleDrive.class.php
@@ -13,11 +13,15 @@ use Google_Service_Drive_Permission;
  */
 class GoogleDrive extends \Quanta\Common\GoogleClient{
 
+    const FILE_READER_ROLE = 'reader';
+    const FILE_COMMENTER_ROLE = 'commenter';
+    const FILE_WRITER_ROLE = 'writer';
+
   
 
     public function __construct(&$env){
        // Call the parent class constructor at the end
-       parent::__construct($env);
+       parent::__construct($env,[Google_Service_Drive::DRIVE]);
        $this->service = new Google_Service_Drive($this->client);
 
     }
@@ -29,15 +33,21 @@ class GoogleDrive extends \Quanta\Common\GoogleClient{
      * @param String $role
      */
     public function changePermissions($fileId, $emails, $role) {
+      try {
         foreach ($emails as $email) {
             $permissions = new Google_Service_Drive_Permission(array(
                 'type' => 'user',
                 'role' => $role,
                 'emailAddress' => $email
             ));
-    
+          
             $this->service->permissions->create($fileId, $permissions);
         }
+      } catch (\Throwable $th) {
+        print_r($th);
+        die();
+      }
+       
     }
 
  
