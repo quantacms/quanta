61183f08d1084a672bb8f35c63b820598bafe3ae
MohamedTaha880 <mohamed.t@storexweb.com>
2024-06-06 10:53:25 -0400

    import file module

--- src/modules/importfile/classes/Common/ImportFileFactory.class.php
+++ src/modules/importfile/classes/Common/ImportFileFactory.class.php
@@ -7,11 +7,40 @@ namespace Quanta\Common;
  */
 class ImportFileFactory {
 
-  /**
-   * Check if the current request is for download pdf.
-   */
-  public static function importFileData(Environment $env){
-   
+
+  public static function importFileData(Environment $env,$vars){
+    $node = $vars['node'];
+    $file = array_pop($vars['uploaded_files']);
+    $fileobj = new FileObject($env, $file, $node);
+    $imported_data = [];
+    if($fileobj && $fileobj->exists){
+        $filename = $fileobj->getNode()->getPath() . '/' . $fileobj->getPath();
+        // Open the CSV file
+        if (!file_exists($filename) || !is_readable($filename)) {
+            return false;
+        }
+        if (($handle = fopen($filename, 'r')) !== false) {
+            $counter = 0;
+            
+            // Loop through each line of the CSV file
+            while (($row = fgetcsv($handle, 1000, ',')) !== false) {
+                $counter++;
+                if($counter === 1) {    continue;}
+                $row_data = [];
+                // print_r($row);
+                for ($i=0; $i <count($row); $i++) { 
+                    $item = $row[$i];
+                    $row_data[$i] = $item ;
+                }
+                $imported_data []=   $row_data;
+            }
+            // Close the file
+            fclose($handle);
+            $vars['imported_data'] = $imported_data;
+            $env->hook('file_imported',$vars);
+        }
+    }
+    return $imported_data;
   }
 
 }
