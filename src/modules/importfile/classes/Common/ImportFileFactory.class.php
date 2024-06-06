31b63942ca009a2495a31b2735a79409cbdc4e2f
MohamedTaha880 <mohamed.t@storexweb.com>
2024-06-06 03:40:57 -0400

    first step in import file

---
+++ src/modules/importfile/classes/Common/ImportFileFactory.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Quanta\Common;
+/**
+ * Class ImportFileFactory
+ *
+ *
+ */
+class ImportFileFactory {
+
+  /**
+   * Check if the current request is for download pdf.
+   */
+  public static function importFileData(Environment $env){
+   
+  }
+
+}
