45a58cb75e8662a511523c2ec6c8c4a862afd544
MohamedTaha880 <mohamed.t@storexweb.com>
2024-06-12 04:00:42 -0400

    fix checkboxes

--- src/modules/api/classes/Qtags/QueryString.qtag.php
+++ src/modules/api/classes/Qtags/QueryString.qtag.php
@@ -12,7 +12,11 @@ class QueryString extends Qtag {
    *   The rendered Qtag.
    */
   public function render() {
-    if (!empty($this->getAttribute('name')) && !empty($_REQUEST[$this->getAttribute('name')])) {
+    if($this->getAttribute('JSON') && !empty($this->getAttribute('name')) && !empty($this->getAttribute('data'))){
+      $request_data = json_decode($_REQUEST[$this->getAttribute('name')]);
+      return $request_data->{$this->getAttribute('data')};
+    }
+    elseif (!empty($this->getAttribute('name')) && !empty($_REQUEST[$this->getAttribute('name')])) {
       return $_REQUEST[$this->getAttribute('name')];
     }
   }
