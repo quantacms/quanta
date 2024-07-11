b4a1cb68c798dde533507585dd95cf5b7ee50048
MohamedTaha880 <mohamed.t@storexweb.com>
2024-07-11 17:03:07 -0400

    minor fixes

--- src/modules/api/classes/Qtags/Separate.qtaq.php
+++ src/modules/api/classes/Qtags/Separate.qtaq.php
@@ -14,13 +14,20 @@ class Separate extends Qtag {
   public function render() {
     $html = '';
     $separator = $this->getAttribute('separator');
+    $is_for_filter = $this->getAttribute('for-filter');
     $exploded_array = explode($separator, $this->getTarget());
     $count = count($exploded_array);
     foreach ($exploded_array as $index => $item) {
-      $title = ucfirst($item);
-      $html .= "[TEXT|tag={$item}:{$title}]";
+      if($is_for_filter){
+        $html .= $item;
+      }
+      else{
+        $title = ucfirst($item);
+        $html .= "[TEXT|tag={$item}:{$title}]";
+      }
+     
       if ($index < $count - 1) {
-        $html .= ', ';
+        $html .= $is_for_filter ? ';' : ', ';
       }
     }
     return $html;
