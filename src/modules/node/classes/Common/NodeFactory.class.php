c6156dd72bf9a427e2667c13b88e5ec5ebd697fb
MohamedTaha880 <mohamed.t@storexweb.com>
2024-07-18 03:19:17 -0400

    add option for hide fallback lang

--- src/modules/node/classes/Common/NodeFactory.class.php
+++ src/modules/node/classes/Common/NodeFactory.class.php
@@ -24,7 +24,7 @@ class NodeFactory {
    * @return Node
    *   The built node object.
    */
-  public static function load(Environment $env, $node_name, $language = NULL, $force_reload = TRUE, $classname = 'Node') {
+  public static function load(Environment $env, $node_name, $language = NULL, $force_reload = TRUE, $classname = 'Node', $fallback_lang = true) {
     static $loaded_nodes;
 
      
@@ -50,9 +50,9 @@ class NodeFactory {
     }
 
     $node->load();
+   
 
-
-    if (!($node->hasTranslation($language))) {
+    if (!($node->hasTranslation($language)) && $fallback_lang) {
       $fallback = Localization::getFallbackLanguage($env);
       $node = new Node($env, $node_name, NULL, 'it');
     }
@@ -76,8 +76,8 @@ class NodeFactory {
    * @return Node
    *   The built node object.
    */
-  public static function loadOrCurrent($env, $node, $language = NULL) {
-    return empty($node) ? NodeFactory::current($env) : NodeFactory::load($env, $node, $language);
+  public static function loadOrCurrent($env, $node, $language = NULL, $fallback_lang = TRUE) {
+    return empty($node) ? NodeFactory::current($env) : NodeFactory::load($env, $node, $language, true, 'Node', $fallback_lang );
   }
 
   /**
@@ -626,7 +626,8 @@ class NodeFactory {
         $value = $value[0];
       }
       $length = (!empty($form_item_data->length) ? $form_item_data->length : 50000);
-      $attributes = array('length' => $length, 'type' => $form_item_data->type, 'required' => $form_item_data->required, 'name' => $form_item, 'value' => $value );
+      $required = $form_item_data->required ? true : null;
+      $attributes = array('length' => $length, 'type' => $form_item_data->type, 'required' => $required, 'name' => $form_item, 'value' => $value );
       $form_state = null;
       $input_item = \Quanta\Common\FormFactory::createInputItem($env, $attributes, $form_state);
       $input_item->validate();
