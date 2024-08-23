e391674195c83590302c7647f1aa3353eac55c5b
MohamedTaha880 <mohamed.t@storexweb.com>
2024-08-23 10:10:36 +0200

    add warning message if the language code not found

--- src/modules/localization/classes/Common/Localization.class.php
+++ src/modules/localization/classes/Common/Localization.class.php
@@ -218,6 +218,14 @@ class Localization {
     if($node->exists && !empty($node->getAttributeJSON('locale_code'))){
       $locale_code = $node->getAttributeJSON('locale_code');
     }
+    else{
+      new Message($env,
+      t('Warning:You should add a language code to the language !language',
+        array('!language' => $lang)
+      ),
+      \Quanta\Common\Message::MESSAGE_WARNING
+    );
+    }
     return $locale_code;
   }
 }
