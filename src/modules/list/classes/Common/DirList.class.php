c2dc0a469d6a1299daefdb8a7047723d387c3e6a
MohamedTaha880 <mohamed.t@storexweb.com>
2024-09-27 09:54:48 +0200

    add remove item function to dirlist

--- src/modules/list/classes/Common/DirList.class.php
+++ src/modules/list/classes/Common/DirList.class.php
@@ -146,4 +146,17 @@ class DirList extends ListObject {
   public function addItem($node) {
 		$this->items[$node->getName()] = $node;
   }
+
+   /**
+   * Remove a node item to the list.
+   *
+   * @param String $key
+   *   The key to be removed from the list.
+   */
+  public function removeItem($key) {
+    if(!isset($this->items[$key])){
+      return;
+    }
+    unset($this->items[$key]); 
+  }
 }
