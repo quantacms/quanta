32b9048de40864621682b14d63733fbd41a92da7
MohamedTaha880 <mohamed.t@storexweb.com>
2024-10-29 13:48:57 +0100

    add options to hide actions from file operations

--- src/modules/file/classes/Qtags/FileOperations.qtag.php
+++ src/modules/file/classes/Qtags/FileOperations.qtag.php
@@ -16,6 +16,8 @@ class FileOperations extends HtmlTag {
     $nodeobj = \Quanta\Common\NodeFactory::loadOrCurrent($this->env, $node_name);
     $img_name = $this->getAttribute('img');
     $img_key = $this->getAttribute('key');
+    $show_delete_btn = !empty($this->getAttribute('delete_btn')) ? $this->getAttribute('delete_btn') == 'true' : true;
+    $show_set_as_thumbnail_btn = !empty($this->getAttribute('set_as_thumbnail_btn')) ? $this->getAttribute('set_as_thumbnail_btn') == 'true' : true;
     $user = \Quanta\Common\UserFactory::Current($this->env);
     $is_anonymous = in_array('anonymous',$user->roles);
     if (!$is_anonymous && \Quanta\Common\NodeAccess::check($this->env, \Quanta\Common\Node::NODE_ACTION_EDIT, array('node' => $nodeobj))) {
@@ -23,6 +25,8 @@ class FileOperations extends HtmlTag {
       $this->attributes['attr-data-img_node'] = $node_name;
       $this->attributes['attr-data-img'] = $img_name;
       $this->attributes['attr-data-img_key'] = $img_key;
+      $this->attributes['attr-data-show-delete-btn'] = $show_delete_btn;
+      $this->attributes['attr-data-show-set-as-thumbnail-btn'] = $show_set_as_thumbnail_btn;
     }
     return parent::render();
   }
