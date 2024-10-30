26162ddd6d4f1875ee41d29166744a0b38d712e7
MohamedTaha880 <mohamed.t@storexweb.com>
2024-10-30 08:13:44 +0100

    add query filter to file operations

--- src/modules/file/classes/Qtags/FileOperations.qtag.php
+++ src/modules/file/classes/Qtags/FileOperations.qtag.php
@@ -17,9 +17,16 @@ class FileOperations extends HtmlTag {
     $img_name = $this->getAttribute('img');
     $img_key = $this->getAttribute('key');
     $show_buttons = !empty($this->getAttribute('show_buttons')) ? $this->getAttribute('show_buttons') : 'delete-btn,set-as-thumbnail-btn';
+    $q_filter = !empty($this->getAttribute('q_filter')) ? $this->getAttribute('q_filter') : false;
+    $q_filter_value = !empty($this->getAttribute('q_filter_value')) ? $this->getAttribute('q_filter_value') : 1;
+
     $user = \Quanta\Common\UserFactory::Current($this->env);
     $is_anonymous = in_array('anonymous',$user->roles);
-    if (!$is_anonymous && \Quanta\Common\NodeAccess::check($this->env, \Quanta\Common\Node::NODE_ACTION_EDIT, array('node' => $nodeobj))) {
+    if (
+      !$is_anonymous &&
+      \Quanta\Common\NodeAccess::check($this->env, \Quanta\Common\Node::NODE_ACTION_EDIT, array('node' => $nodeobj)) &&
+      (!$q_filter || ($q_filter && (isset($_REQUEST[$q_filter]) && $_REQUEST[$q_filter] == $q_filter_value)))
+      ) {
       $this->addClass('file-operation');
       $this->attributes['attr-data-img_node'] = $node_name;
       $this->attributes['attr-data-img'] = $img_name;
