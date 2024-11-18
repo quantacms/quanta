c07bfc4d3fc6a749a42c22dc8fbd0a8a80a7c53c
Mahmoud Alaa <mahmoud.a@storexweb.com>
2024-11-18 16:51:22 +0200

    add max resolution to file upload

--- src/modules/file/classes/Qtags/ResolutionMessage.qtag.php
+++ src/modules/file/classes/Qtags/ResolutionMessage.qtag.php
@@ -10,10 +10,19 @@ class ResolutionMessage extends HtmlTag {
     protected $html_tag = 'p';
 
     public function render() {
-        $resolution = $this->getTarget();
-        if(!empty($resolution)){
+        $min_resolution = $this->getAttribute('min');
+        $max_resolution = $this->getAttribute('max');
+        if(!empty($min_resolution)|| !empty($max_resolution)){
             $this->addClass('shadow-desc');
-            $this->html_body = "[TEXT|tag=resolution-description:dimensioni consigliate:]" . $resolution . " pixel";
+            $this->html_body = "[TEXT|tag=resolution-description:dimensioni consigliate:]";
+            if (!empty($min_resolution)) {
+                $this->html_body .= "[TEXT|tag=min:minima]:" . $min_resolution;
+            }
+            if (!empty($max_resolution)) {
+                $this->html_body .= "[TEXT|tag=max:massimo]:" . $max_resolution;
+
+            }
+             $this->html_body .= " pixel";
             return parent::render();
         }
        
