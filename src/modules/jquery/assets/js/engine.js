74e19599753d8272c2621a02b619a4903bc1e2ea
MohamedTahanNew <mahamad.t11@storexweb.team>
2025-01-23 11:03:43 +0100

    remove shadow cpmmits

--- src/modules/jquery/assets/js/engine.js
+++ src/modules/jquery/assets/js/engine.js
@@ -57,11 +57,8 @@ var actionSuccess = function(data) {
     return false;
   }
   
-  if(data?.shadow){
+  if(data.shadow){
     openShadow(data.shadow);
-  }
-  else if(data?.close){
-    closeShadow(true);
   }
   // TODO: better way to display errors.
   if (data.errors) {
