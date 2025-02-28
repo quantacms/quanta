7f62af18e3f4dfb9fa1263703015ee4b86908d98
Aldo Tripiciano <aldo.tripiciano@gmail.com>
2025-02-28 10:25:10 +0100

    Update Environment.class.php

--- src/modules/environment/classes/Common/Environment.class.php
+++ src/modules/environment/classes/Common/Environment.class.php
@@ -562,7 +562,6 @@ class Environment extends DataContainer {
    *   The result of the node search.
    */
   private function findNodePath($folder) {
-    print $folder . '<br>';
     // TODO: cleaner way to exclude folders in _modules.
     $findcmd = 'find ' . $this->dir['docroot'] . '/ -type d -name "' . $folder . '" -not -path */_modules* -not -path *.git*';
     // TODO: sometimes getting empty folder. Why? Temporary fix.
