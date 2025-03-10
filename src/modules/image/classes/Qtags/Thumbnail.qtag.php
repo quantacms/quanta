83fd7f7f5e786a9344810773d84a8976d123e821
Mahmoud Alaa <mahmoud.a@storexweb.com>
2025-03-10 09:12:47 +0200

    add inline documentation

--- src/modules/image/classes/Qtags/Thumbnail.qtag.php
+++ src/modules/image/classes/Qtags/Thumbnail.qtag.php
@@ -18,23 +18,33 @@ class Thumbnail extends ImgThumb {
     $thumbnail = $node->getThumbnail();
     $fallback = $this->getAttribute('fallback');
     $target = $thumbnail;
+    // Check if the thumbnail is empty and a fallback value is provided
     if (! $thumbnail && !empty($fallback)) {
+      // If the fallback is set to 'first_image', attempt to find the first image file
       if ($fallback === 'first_image') {
+        // Set attributes to filter for images only
         $this->attributes['file_types'] = 'image';
         $this->attributes['clean'] = true;
-        $filelist = new \Quanta\Common\FileList($this->env, $this->getTarget(), null, $this->attributes, 'list');
 
-        // make sure it is image
-        foreach ($filelist->getItems() as $file) {
-          if ($file->type === 'image') {
-            $target = $file->getName();
-            break;
+        // Get the list of files associated with the node, filtered by the specified attributes
+        $filelist = new \Quanta\Common\FileList($this->env, $node->getName(), null, $this->attributes, 'list');
+
+        // Retrieve the filtered list of files
+        $files = $filelist->getItems();
+
+        // Loop through the files to find the first one with type 'image'
+        foreach ($files as $file) {
+          if ($file->type === 'image') {  // Ensure the file is an image
+            $target = $file->getName();  // Set the target to the name of the first image found
+            break;  // Stop searching after finding the first image
           }
         }
       } else {
+        // If the fallback is not 'first_image', use the provided fallback value as the target
         $target = $fallback;
       }
     }
+    // Set the target using the determined value
     $this->setTarget($target);
     $html = parent::render();
     if (empty($this->getAttribute('link')) || $this->getAttribute('link') != 'false') {
